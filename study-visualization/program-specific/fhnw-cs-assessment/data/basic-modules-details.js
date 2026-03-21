3d7bc14536c7bcb84bfcbff852e3a96ac86f7404
Kristian Lubina <kristian.lubina@outlook.com>
2026-03-22 00:11:47 +0100

    Add FHNW study materials and visualization assets
    Import a large set of FHNW course materials and related frontend tooling: module details/pages (txt, pdf, docx, pptx), assessment module files, JavaFX example projects for OOP modules, the HolidayFinder PHP example, and study-visualization frontend (HTML/CSS/JS templates, data and config). Also add project miscellanea (VSCode settings, index.html) to seed the repository for the study visualization and module documentation site.

---
+++ study-visualization/program-specific/fhnw-cs-assessment/data/basic-modules-details.js
@@ -0,0 +1,35 @@
+/* ==== FHNW BSc INFORMATIK (NUR ASSESSMENT) MODULE DETAILS ==== */
+/* Optional details used by module material buttons/links */
+
+window.StudiengangModuleDetails = {
+  "Programmieren 1": {
+    vorlesungsmaterial: "https://www.fhnw.ch/de/studium/module/9794369",
+  },
+  "User Centered Design and Requirements Engineering": {
+    vorlesungsmaterial: "https://www.fhnw.ch/de/studium/module/9795670",
+  },
+  Informationssysteme: {
+    vorlesungsmaterial: "https://www.fhnw.ch/de/studium/module/9795679",
+  },
+  "Mathematische Grundlagen der Informatik": {
+    vorlesungsmaterial: "https://www.fhnw.ch/de/studium/module/9813296",
+  },
+  "Software Engineering Fundamentals": {
+    vorlesungsmaterial: "https://www.fhnw.ch/de/studium/module/9813303",
+  },
+  "Individuelles Softwareprojekte": {
+    vorlesungsmaterial: "https://www.fhnw.ch/de/studium/module/9795668",
+  },
+  "System Engineering": {
+    vorlesungsmaterial: "https://www.fhnw.ch/de/studium/module/9797596",
+  },
+  "Data & AI Fundation": {
+    vorlesungsmaterial: "https://www.fhnw.ch/de/studium/module/9795684",
+  },
+  "Programmieren 2": {
+    vorlesungsmaterial: "https://www.fhnw.ch/de/studium/module/9794384",
+  },
+  "Lineare Algebra": {
+    vorlesungsmaterial: "https://www.fhnw.ch/de/studium/module/9813291",
+  },
+};
